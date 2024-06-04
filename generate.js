8e9a8a6adea1a4c80da2c4244f1d9c912c902290
Alexander Kurz <alexhkurz@gmail.com>
2024-06-03 18:24:58 -0700

    Removed rankObjects function from generate.js and added its call in index.html.

--- generate.js
+++ generate.js
@@ -34,15 +34,3 @@ function writeDataToCsv(data) {
 const data = generateData();
 x = rankObjects(data);
 writeDataToCsv(data);
-
-function rankObjects(data) {
-    let x = [];
-    for (let row of data) {
-        let sum = 0;
-        for (let i = 0; i < n; i++) {
-            sum += c[i] * row[`a${i+1}`];
-        }
-        x.push(sum);
-    }
-    return x;
-}
