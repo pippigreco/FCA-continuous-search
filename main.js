5532ad16a45a1cc7792ff293601b4d19f692b62b
Alexander Kurz <alexhkurz@gmail.com>
2024-06-03 18:22:36 -0700

    Implemented rankObjects function to calculate sum of object properties in main.js.

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // Main algorithm implementation will go here
 console.log("main.js loaded");
+
+function rankObjects(data) {
+    let x = [];
+    for (let row of data) {
+        let sum = 0;
+        for (let i = 0; i < n; i++) {
+            sum += c[i] * row[`a${i+1}`];
+        }
+        x.push(sum);
+    }
+    return x;
+}
+
